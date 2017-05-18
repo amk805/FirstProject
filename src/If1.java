313ebea9d40a4e270b3035fa5fcff9ecf93f0eb8
amk805 <appalaneni99@gmail.com>
2017-05-18 00:32:20 -0400

    bug

---
+++ src/If1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class If1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a;
+     Scanner in = new Scanner(System.in);
+     System.out.println("Enter the value of a and hit return");
+     a= in.nextInt();
+     if(a<100){
+    	 System.out.println("number is less than 100");
+     
+     }
+     
+     else{
+    	 System.out.println("number is not less than 100");
+     }
+	
+	}
+
+}
